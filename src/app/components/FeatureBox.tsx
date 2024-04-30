7d6cecaff1301a044bbbeaef2049a70a54374316
samet <sametcn99@gmail.com>
2024-04-30 20:16:10 +0300

    Refactor FeatureBox component styles and update layout

--- src/app/components/FeatureBox.tsx
+++ src/app/components/FeatureBox.tsx
@@ -8,8 +8,8 @@ export default function FeatureBox({
   description: string;
 }) {
   return (
-    <div className="flex w-full flex-col rounded-2xl border bg-gray-300 p-3 text-black shadow-2xl shadow-green-950 sm:h-64 sm:w-64">
-      <h1 className="text-2xl font-bold">{title}</h1>
+    <div className="flex w-full flex-col rounded-2xl bg-green-950 bg-opacity-30 p-6 text-white shadow-2xl sm:h-64 sm:w-64">
+      <h1 className="text-xl font-bold">{title}</h1>
       <p>{description}</p>
     </div>
   );
